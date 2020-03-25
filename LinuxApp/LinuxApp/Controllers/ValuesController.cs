91499248f344e8bdfccb0ce33cc5b44dfa4e8299
Aleksandr Kusughev <kugushew@gmail.com>
2020-03-25 12:32:38 +0300

    add projects

---
+++ LinuxApp/LinuxApp/Controllers/ValuesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LinuxApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(IHttpClientFactory httpClientFactory, ILogger<ValuesController> logger)
+        {
+            _httpClientFactory = httpClientFactory;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("http://localhost:5041/api/values");
+
+            return Ok(await response.Content.ReadAsStringAsync());
+        }
+    }
+}
